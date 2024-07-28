1c2ef883212daf053646efd4b0cd4dcfb6b78544
Codecraftian <suryapratappal30@gmail.com>
2024-07-28 20:24:26 +0530

    database is connected successfully

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
-
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import {app} from "./app.js"
 
 dotenv.config({
   path: "./env"
 })
 
 connectDB()
+.then(()=>{
+  app.listen(process.env.PORT || 8000 , () =>{
+    console.log(`Server is running on port ${process.env.PORT || 8000}`)
+  })
+
+  app.on("ERROR",(error)=> {
+    console.log("DATABASE IS NOT CONNECTED",error);
+    throw error;
+  })
+})
+.catch((error)=>{
+  console.log("Error connecting to database", error)
+})
 
 
 
